284526abf1b574e9edee1350c3d7830f3c23772c
Amin El-Rifai <amin@users.noreply.github.com>
2026-01-15 11:53:18 +0100

    Solutions C#-1

---
+++ C#/Week1/Assignment5/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+var A = 50;
+var B = 45;
+var C = 90;
+
+List<int> cart = [A, B, C];
+var price = cart.Sum() - cart.Min();
+
+Console.WriteLine($"Your total is ${price}");
---
+++ C#/Week1/Assignment6/Program.cs
@@ -0,0 +1,8 @@
+﻿var start = 3;
+var stop = 1278;
+
+while (start <= stop)
+{
+    Console.WriteLine(start);
+    start = (start + 1) * 2;
+}   
